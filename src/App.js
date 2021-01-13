01abecf3fc03bed37f93104aa601bb2c1a83af5d
Olawale <olawale.akinodanye@gmail.com>
2021-01-13 22:37:05 +0100

    Final stuff

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import "./App.css";
 import Navbar from "./components/pages/Navbar";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./components/pages/HomePage/Home";
+import Footer from "./components/pages/Footer.js/Footer";
+import Services from "./components/pages/Services/Services";
+import Products from "./components/pages/Products/Products";
+import SignUp from "./components/pages/SignUp/SignUp";
 
 function App() {
   return (
@@ -8,8 +13,12 @@ function App() {
       <div className="App">
         <Navbar />
         <Switch>
-          <Route path="/" />
+          <Route path="/" exact component={Home} />
+          <Route path="/services" component={Services} />
+          <Route path="/products" component={Products} />
+          <Route path="/sign-up" component={SignUp} />
         </Switch>
+        <Footer />
       </div>
     </BrowserRouter>
   );
--- src/components/pages/HomePage/Home.js
+++ src/components/pages/HomePage/Home.js
@@ -1,11 +1,16 @@
 import React from "react";
+import Pricing from "../../Pricing";
 import HeroSection from "../HeroSection";
-import { homeObjOne } from "./Data";
+import { homeObjOne, homeObjFour, homeObjThree, homeObjTwo } from "./Data";
 
 function Home() {
   return (
     <React.Fragment>
       <HeroSection {...homeObjOne} />
+      <HeroSection {...homeObjThree} />
+      <HeroSection {...homeObjTwo} />
+      <Pricing />
+      <HeroSection {...homeObjFour} />
     </React.Fragment>
   );
 }
