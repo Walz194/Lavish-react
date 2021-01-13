fe6002704e26de77d22ad0857010545d9070693d
Olawale <olawale.akinodanye@gmail.com>
2021-01-13 22:57:00 +0100

    Now i'm really done

--- src/components/pages/Products/Products.js
+++ src/components/pages/Products/Products.js
@@ -1,13 +1,11 @@
 import React from "react";
 import HeroSection from "../HeroSection";
-import { homeObjTwo } from "./Data";
-import Pricing from "../../Pricing";
+import { homeObjFour } from "./Data";
 
 function Products() {
   return (
     <>
-      <Pricing />
-      <HeroSection {...homeObjTwo} />
+      <HeroSection {...homeObjFour} />
     </>
   );
 }
--- src/components/pages/SignUp/SignUp.js
+++ src/components/pages/SignUp/SignUp.js
@@ -1,12 +1,11 @@
 import React from "react";
 import HeroSection from "../HeroSection";
-import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour } from "./Data";
+import { homeObjThree } from "./Data";
 import Pricing from "../../Pricing";
 
 function SignUp() {
   return (
     <>
-      <HeroSection {...homeObjOne} />
       <HeroSection {...homeObjThree} />
     </>
   );
